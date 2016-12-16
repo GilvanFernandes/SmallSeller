da540d1e9d15f7b8fe567d9ad1afbef5173b1081
Gilvan Fernandes <gil-f-o@hotmail.com>
2016-12-16 15:47:06 -0200

    Dados iniciais após restore do banco

---
+++ data/dump_data.sql
@@ -0,0 +1,2 @@
+INSERT INTO `smallseller`.`usuarios` (`nome`, `email`, `senha`) VALUES ('Genérico', 'generico@teste.com', '6c01e8a2f7d60b74be4d5e2713a1fa0c4108e1c4');
+INSERT INTO `smallseller`.`usuarios` (`nome`, `email`, `senha`, `tipo`) VALUES ('Gilvan Fernandes', 'gil-f-o@hotmail.com', '802df9b5ba76be436586db201349abd35f6cc0a2', '1');
