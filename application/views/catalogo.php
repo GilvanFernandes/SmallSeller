93bf87dfd13ccfd59c373d2188af5acc09835f43
Gilvan Fernandes <gil-f-o@hotmail.com>
2016-12-16 15:45:41 -0200

    Interface dos produtos e formulário de contato com administrador dos produtos

--- application/views/catalogo.php
+++ application/views/catalogo.php
@@ -2,210 +2,89 @@
 		<div class="sixteen columns services_div">
 			<div class="Services_1_title_div">
 				<h3>
-					Produto do Dia
+					Produtos do Dia | <?php  echo date('d/m/Y'); ?>
 				</h3>
 			</div>
 			<div class="clearfix"></div>
-			<p class="services_tagline">
-				Lorem ipsum dolor sit amet, consectetur adipisicing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.
-			</p>
+
 		</div>
 		<div class="services_boxes services_boxes2">
 			<div class="sixteen columns ">
 
-			<div class="four columns alpha services_box team_box">
-				 <img class="team_image" src="content_images/team_4.jpg" alt="">
-				 <div class="box_text_div">
-				 	<h3 class="box_title">
-				 		Alex Brown
-				 	</h3>
-				 	<div class="team_item_tagline">
-				 		CEO AND FOUNDER
-				 	</div>
-				 	<p class="box_text">
-				 		Lorem ipsum dolorsed avec malior from etlie aliqua.
-				 	</p>
-				 	<div class="team_item_social_div">
-				 	<span class="team_item_social ">
-						<a href="#" class="footer_social facebook-c"></a>
-						<a href="#" class="footer_social twitter-c"></a>
-						<a href="#" class="footer_social forrst-c"></a>
-						<a href="#" class="footer_social dribbble-c"></a>
-					</span>
-				 	</div>
-				 </div>
-			</div>
-			<div class="sep"><div class="clearfix"></div></div>
-			<div class="four columns  services_box team_box">
-				 <img class="team_image" src="content_images/team_9.jpg" alt="">
-				 <div class="box_text_div">
-				 	<h3 class="box_title">
-				 		Jena Robien
-				 	</h3>
-				 	<div class="team_item_tagline">
-				 		SENIOR DEVELOPER
-				 	</div>
-				 	<p class="box_text">
-				 		Lorem ipsum dolorsed avec malior from etlie aliqua.
-				 	</p>
-				 	<div class="team_item_social_div">
-				 	<span class="team_item_social ">
-						<a href="#" class="footer_social facebook-c"></a>
-						<a href="#" class="footer_social twitter-c"></a>
-						<a href="#" class="footer_social forrst-c"></a>
-						<a href="#" class="footer_social dribbble-c"></a>
-					</span>
-				 	</div>
-				 </div>
-			</div>
-			<div class="sep"><div class="clearfix"></div></div>
-			<div class="four columns  services_box team_box">
-				 <img class="team_image" src="content_images/team_6.jpg" alt="">
-				 <div class="box_text_div">
-				 	<h3 class="box_title">
-				 		James Normand
-				 	</h3>
-				 	<div class="team_item_tagline">
-				 		GRAPHIC DESIGNER
-				 	</div>
-				 	<p class="box_text">
-				 		Lorem ipsum dolorsed avec malior from etlie aliqua.
-				 	</p>
-				 	<div class="team_item_social_div">
-				 	<span class="team_item_social ">
-						<a href="#" class="footer_social facebook-c"></a>
-						<a href="#" class="footer_social twitter-c"></a>
-						<a href="#" class="footer_social forrst-c"></a>
-						<a href="#" class="footer_social dribbble-c"></a>
-					</span>
-				 	</div>
-				 </div>
-			</div>
-			<div class="sep"><div class="clearfix"></div></div>
-			<div class="four columns omega services_box team_box">
-				 <img class="team_image" src="content_images/team_7.jpg" alt="">
-				 <div class="box_text_div">
-				 	<h3 class="box_title">
-				 		Julia Hakmen
-				 	</h3>
-				 	<div class="team_item_tagline">
-				 		SUPERVISOR
-				 	</div>
-				 	<p class="box_text">
-				 		Lorem ipsum dolorsed avec malior from etlie aliqua.
-				 	</p>
-				 	<div class="team_item_social_div">
-				 	<span class="team_item_social ">
-						<a href="#" class="footer_social facebook-c"></a>
-						<a href="#" class="footer_social twitter-c"></a>
-						<a href="#" class="footer_social forrst-c"></a>
-						<a href="#" class="footer_social dribbble-c"></a>
-					</span>
-				 	</div>
-				 </div>
-			</div>
+
+				<?php if(!isset($rsProdutosDia)): ?>
+
+					<p class="services_tagline">
+						Sem novidades no momento =(
+					</p>
+
+				<?php else: ?>
+
+					<?php foreach ($rsProdutosDia as $sProduto): ?>
+
+					<div class="sep"><div class="clearfix"></div></div>
+					<div class="four columns  services_box team_box">
+						 <img class="team_image" src="<?php echo base_url()."upload/produtos/".$sProduto->foto;?>" alt="<?php echo $sProduto->nome;?>">
+						 <div class="box_text_div">
+						 	<h3 class="box_title">
+						 		<?php echo $sProduto->nome;?>
+						 	</h3>
+
+						 	<p class="box_text">
+						 		 R$ <?php echo $sProduto->valor;?><br /><?php echo $sProduto->detalhes;?>
+						 	</p>
+						 	<div class="team_item_social_div">
+						 	<span class="team_item_social ">
+								<a href="#" class="footer_social facebook-c"></a>
+								<a href="#" class="footer_social twitter-c"></a>
+							</span>
+						 	</div>
+						 </div>
+					</div>
+
+					<?php endforeach;?>
+
+				<?php endif;?>
+
+
 			<div class="clearfix"></div>
 		</div>
 
+		</div>
 		<div class="clearfix"></div>
-		<div class="sixteen columns ">
 
-			<div class="four columns alpha services_box team_box">
-				 <img class="team_image" src="content_images/team_1.jpg" alt="">
-				 <div class="box_text_div">
-				 	<h3 class="box_title">
-				 		Alice Parce
-				 	</h3>
-				 	<div class="team_item_tagline">
-				 		SECRETARY
-				 	</div>
-				 	<p class="box_text">
-				 		Lorem ipsum dolorsed avec malior from etlie aliqua.
-				 	</p>
-				 	<div class="team_item_social_div">
-				 	<span class="team_item_social ">
-						<a href="#" class="footer_social facebook-c"></a>
-						<a href="#" class="footer_social twitter-c"></a>
-						<a href="#" class="footer_social forrst-c"></a>
-						<a href="#" class="footer_social dribbble-c"></a>
-					</span>
-				 	</div>
-				 </div>
-			</div>
-			<div class="sep"><div class="clearfix"></div></div>
-			<div class="four columns  services_box team_box">
-				 <img class="team_image" src="content_images/team_3.jpg" alt="">
-				 <div class="box_text_div">
-				 	<h3 class="box_title">
-				 		Jonny Deep
-				 	</h3>
-				 	<div class="team_item_tagline">
-				 		WEB DEVELOPMENT
-				 	</div>
-				 	<p class="box_text">
-				 		Lorem ipsum dolorsed avec malior from etlie aliqua.
-				 	</p>
-				 	<div class="team_item_social_div">
-				 	<span class="team_item_social ">
-						<a href="#" class="footer_social facebook-c"></a>
-						<a href="#" class="footer_social twitter-c"></a>
-						<a href="#" class="footer_social forrst-c"></a>
-						<a href="#" class="footer_social dribbble-c"></a>
-					</span>
-				 	</div>
-				 </div>
-			</div>
-			<div class="sep"><div class="clearfix"></div></div>
-			<div class="four columns  services_box team_box">
-				 <img class="team_image" src="content_images/team_8.jpg" alt="">
-				 <div class="box_text_div">
-				 	<h3 class="box_title">
-				 		Liza Barine
-				 	</h3>
-				 	<div class="team_item_tagline">
-				 		SUPPORT DESK
-				 	</div>
-				 	<p class="box_text">
-				 		Lorem ipsum dolorsed avec malior from etlie aliqua.
-				 	</p>
-				 	<div class="team_item_social_div">
-				 	<span class="team_item_social ">
-						<a href="#" class="footer_social facebook-c"></a>
-						<a href="#" class="footer_social twitter-c"></a>
-						<a href="#" class="footer_social forrst-c"></a>
-						<a href="#" class="footer_social dribbble-c"></a>
-					</span>
-				 	</div>
-				 </div>
-			</div>
-			<div class="sep"><div class="clearfix"></div></div>
-			<div class="four columns omega services_box team_box">
-				 <img class="team_image" src="content_images/team_5.jpg" alt="">
-				 <div class="box_text_div">
-				 	<h3 class="box_title">
-				 		Megan Fox
-				 	</h3>
-				 	<div class="team_item_tagline">
-				 		SUPPORT DESK
-				 	</div>
-				 	<p class="box_text">
-				 		Lorem ipsum dolorsed avec malior from etlie aliqua.
-				 	</p>
-				 	<div class="team_item_social_div">
-				 	<span class="team_item_social ">
-						<a href="#" class="footer_social facebook-c"></a>
-						<a href="#" class="footer_social twitter-c"></a>
-						<a href="#" class="footer_social forrst-c"></a>
-						<a href="#" class="footer_social dribbble-c"></a>
-					</span>
-				 	</div>
-				 </div>
+		<div class="sixteen columns services_div">
+			<div class="Services_1_title_div">
+				<h3>
+					Produtos dos últimos três dias =)
+				</h3>
 			</div>
 			<div class="clearfix"></div>
-		</div>
-
-		<div class="clearfix"></div>
 
 		</div>
+		<div class="services_boxes services_boxes2">
+			<div class="sixteen columns ">
+
+			<?php foreach ($rsProdutosDiasAnteriores as $sProduto): ?>
+
+				<div class="sep"><div class="clearfix"></div></div>
+				<div class="four columns  services_box team_box">
+					 <img class="team_image" src="<?php echo base_url()."upload/produtos/".$sProduto->foto;?>" alt="<?php echo $sProduto->nome;?>">
+					 <div class="box_text_div">
+						<h3 class="box_title">
+							<?php echo $sProduto->nome;?>
+						</h3>
+						<p class="box_text">
+							<?php echo $sProduto->detalhes;?>
+						</p>
+					 </div>
+				</div>
+
+				<?php endforeach;?>
+		</div>
+
+		</div>
 		<div class="clearfix"></div>
+
+
 	</div>
